c4bb90706dc2a6c252aca1e67fff8c720f508f65
Steven Skoczen <steven@quantumimagery.com>
2010-11-07 22:48:32 -0800

    hopefully saner structure for namespaces

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup(
     name = "qi-toolkit",
-    version = "0.1",
+    version = "0.1.1",
     packages = find_packages(),
-    scripts = ['fabbase.py'],
+    scripts = ['qi_toolkit'],
 
 )
