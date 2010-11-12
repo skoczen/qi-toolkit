b0aafae31357cbfa8527ce31ddd536ae8374ce54
Steven Skoczen <steven@quantumimagery.com>
2010-11-12 00:02:41 -0800

    better setup

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+#/usr/bin/env python
+import os
 from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+SOURCE_DIR = os.path.join(ROOT_DIR)
+
 setup(
     name = "qi-toolkit",
-    version = "0.2.1",
+    version = "0.2.2",
     packages = find_packages(),
+    zip_safe = False,
 )
