8a80db2d5497ee97c1e70542b588aedf59c53e24
Steven Skoczen <steven@quantumimagery.com>
2010-11-29 20:01:51 -0800

    Really need a test project for this stuff.

--- qi_toolkit/context_processors.py
+++ qi_toolkit/context_processors.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-def add_env_to_request(self, request):
+def add_env_to_request(request):
     "Adds the ENV context to the template"
     try:
         return {'ENV' : settings.ENV }
