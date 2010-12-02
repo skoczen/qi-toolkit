340812dbaa049db203a3d175dad2666a63f4bb2b
Steven Skoczen <steven@quantumimagery.com>
2010-12-01 21:29:50 -0800

    added favicon supporet

--- qi_toolkit/urls.py
+++ qi_toolkit/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls.defaults import *
 import robots
+from django.conf import settings
 
 
 urlpatterns = patterns('',          
     url(r'^robots.txt',     robots.robots_txt,          name='robots_txt'),
 )
+
+if "FAVICON_URL" in settings:
+    urlpatterns += patterns('',          
+	    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': settings.FAVICON_URL}),
+    )
