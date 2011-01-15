68d70c0ce61daa75af705a887315bf800a57956e
Steven Skoczen <steven@quantumimagery.com>
2011-01-15 13:31:59 -0800

    favicon env added

--- qi_toolkit/context_processors.py
+++ qi_toolkit/context_processors.py
@@ -6,3 +6,10 @@ def add_env_to_request(request):
         return {'ENV' : settings.ENV }
     except:
         return {}
+
+def add_favicon_to_request(request):
+    "Adds the FAVICON context to the template"
+    try:
+        return {'FAVICON' : settings.FAVICON }
+    except:
+        return {}    
