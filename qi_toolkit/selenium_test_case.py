9cf7429a0a1eda440d2a6d7488a2c24b400d7947
Steven Skoczen <steven@quantumimagery.com>
2011-03-17 01:03:43 -0700

    added a conservative selenium test case

--- qi_toolkit/selenium_test_case.py
+++ qi_toolkit/selenium_test_case.py
@@ -1,5 +1,7 @@
 try:
     from djangosanetesting.cases import SeleniumTestCase
+    from django.core.management import call_command
+    from helpers import silence_print, unsilence_print
     class QiSeleniumTestCase(SeleniumTestCase):
         selenium_fixtures = []
         
@@ -9,6 +11,20 @@ try:
 
         def tearDown(self, *args, **kwargs):
             self.assertEqual([], self.verificationErrors)
+            super(QiSeleniumTestCase,self).tearDown(*args, **kwargs)
+
+    class QiConservativeSeleniumTestCase(SeleniumTestCase):
+        selenium_fixtures = []
+        
+        def setUp(self, *args, **kwargs):
+            self.verificationErrors = []
+            super(QiSeleniumTestCase,self).setUp(*args, **kwargs)
+
+        def tearDown(self, *args, **kwargs):
+            self.assertEqual([], self.verificationErrors)
+            silence_print()
+            call_command('flush', interactive=False)
+            unsilence_print()
             super(QiSeleniumTestCase,self).tearDown(*args, **kwargs)
 
 except:
