505f253b06ac612eac2b370f5f78f686e3937214
Steven Skoczen <steven@quantumimagery.com>
2011-04-22 01:47:35 -0700

    more configurability for robots

--- qi_toolkit/robots.py
+++ qi_toolkit/robots.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.conf import settings
 
-def robots_txt(request):
+def robots_txt(request, allow=False):
     try:
         if settings.ENV == "STAGING" or settings.ENV == "DEV":
             return HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")
     except:
         pass
-
-    return HttpResponse("User-agent: *\nDisallow: ", mimetype="text/plain")
+    if allow:
+        return HttpResponse("User-agent: *\nAllow: ", mimetype="text/plain")
+    else:
+        return HttpResponse("User-agent: *\nDisallow: ", mimetype="text/plain")
